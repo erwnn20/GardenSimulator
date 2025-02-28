3978c1c76a3ef5bbbd6d3447ac374c8c44dffb1e
verwann <erwann.varlet@ynov.com>
2025-02-28 12:16:40 +0100

    add save and data class

---
+++ data/save.py
@@ -0,0 +1,41 @@
+import pickle
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from plants.garden import Garden
+
+
+@dataclass
+class Save:
+    name: str
+    user: 'Data'
+    garden: 'Garden'
+
+    def __str__(self) -> str:
+        return (f'{self.name}'
+                '\n' + '\n'.join(
+            [f'  {key.capitalize()}: {value.str()}' for key, value in vars(self).items()
+             if isinstance(value, Data)]))
+
+
+class Data(ABC):
+    _dir: str = '/data/saves/'
+
+    @abstractmethod
+    def str(self) -> str:
+        pass
+
+    @staticmethod
+    def save(filename: str, data: dict[str, Save]) -> None:
+        with open(f'{Data._dir}{filename}', 'wb') as file:
+            pickle.dump(data, file)
+
+    @staticmethod
+    def load(filename: str) -> dict[str, Save]:
+        try:
+            with open(f'{Data._dir}{filename}', 'rb') as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            return {}
